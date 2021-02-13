bce15890e81746a01fe6158491d486c94dff4a60
marochan2629 <iwata26th0776heartasone@gmail.com>
2021-02-13 06:52:07 +0000

    Create user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
   get '/home', to:'static_pages#home'
-  get '/signup', to:'users#new'
   get '/login', to:'static_pages#login'
   get '/about_hsp', to:'static_pages#about_hsp'
+  get '/signup', to:'users#new'
+  resources :users
 end
