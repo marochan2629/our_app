93b1fe41714cf20d15a679fd94b6c062c4e4538a
marochan2629 <iwata26th0776heartasone@gmail.com>
2021-05-16 08:59:09 +0000

    クチコミ投稿機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :logged_in_user, only: [:create]
   def index
+    @my_facility = MyFacility.find(params[:my_facility_id])
+    @reviews = @my_facility.reviews
   end
   
   def create
     @review = Review.new(review_params) 
     @review.user_id = current_user.id
     if @review.save
-      redirect_to my_facilities_path(@review.my_facility)
+      redirect_back(fallback_location: root_path)
     else
       @my_facility = MyFacility.find(params[:id])
       render "my_facilities/show"
--- app/models/my_facility.rb
+++ app/models/my_facility.rb
@@ -14,4 +14,20 @@ class MyFacility < ApplicationRecord
      validates :email
    end
   end
+  
+  def avg_score
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f
+    else
+      0.0
+    end
+  end
+
+  def review_score_percentage
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f*100/5
+    else
+      0.0
+    end
+  end
 end
